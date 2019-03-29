cc8dd45f2124b9ba434bd5779dc97bba808347e8
Leonardo Oliveira <leozartino@gmail.com>
2019-03-29 12:38:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {bolo: 'formigueiro'}
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object,{[key]:value})
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  object[key] = value
+  return object;
+}
