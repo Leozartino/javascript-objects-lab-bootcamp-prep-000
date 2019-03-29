f7117ca9f7acde35795c0966f9892244287388cb
Leonardo Oliveira <leozartino@gmail.com>
2019-03-29 12:41:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ function deleteFromObjectByKey(object,key){
   return delete objetoAtualizado.key;
 }
 
-function 
+function destructivelyDeleteFromObjectByKey(object, key){
+  return delete object.key;
+}
